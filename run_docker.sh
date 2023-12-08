ebaf3e7063000c4b5679dba4abf54df1c2ad6c96
Zihan Zhao <zihan.j.zhao@gmail.com>
2023-12-08 00:02:55 -0600

    Add edge_4k test case; specify cpu limit

--- run_docker.sh
+++ run_docker.sh
@@ -2,12 +2,14 @@
 
 for i in {1..2}
 do
-    docker run --rm -v "$PWD/Py310":/home/Results deb/python:3.10 linear_regression
-    docker run --rm -v "$PWD/Py310":/home/Results deb/python:3.10 grayscale_4k
-    docker run --rm -v "$PWD/Py310":/home/Results deb/python:3.10 db_operations
+    docker run --rm -v "$PWD/Py310":/home/Results --cpus 4 deb/python:3.10 linear_regression
+    docker run --rm -v "$PWD/Py310":/home/Results --cpus 4 deb/python:3.10 grayscale_4k
+    docker run --rm -v "$PWD/Py310":/home/Results --cpus 4 deb/python:3.10 edge_4k
+    docker run --rm -v "$PWD/Py310":/home/Results --cpus 4 deb/python:3.10 db_operations
 
-    docker run --rm -v "$PWD/Py312":/home/Results deb/python:3.12 linear_regression
-    docker run --rm -v "$PWD/Py312":/home/Results deb/python:3.12 grayscale_4k
-    docker run --rm -v "$PWD/Py312":/home/Results deb/python:3.12 db_operations
+    docker run --rm -v "$PWD/Py312":/home/Results --cpus 4 deb/python:3.12 linear_regression
+    docker run --rm -v "$PWD/Py312":/home/Results --cpus 4 deb/python:3.12 grayscale_4k
+    docker run --rm -v "$PWD/Py312":/home/Results --cpus 4 deb/python:3.12 edge_4k
+    docker run --rm -v "$PWD/Py312":/home/Results --cpus 4 deb/python:3.12 db_operations
 done
 
